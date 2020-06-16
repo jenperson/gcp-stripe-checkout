daa8d72a74045e957784d76ae14d9aa79d5527d2
Jen Person <personjeh@gmail.com>
2020-06-16 10:19:33 -0500

    cleaning up code formatting

--- part-3-set-up-shop-website/public/index.js
+++ part-3-set-up-shop-website/public/index.js
@@ -1,5 +1,18 @@
-const stripe = Stripe('pk_test_ABCde');
+// Copyright 2020 Google LLC
 
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const stripe = Stripe('pk_test_ABCde');
 const db = firebase.firestore();
 const sockRef = db.collection('socks');
 let selectedProduct;
